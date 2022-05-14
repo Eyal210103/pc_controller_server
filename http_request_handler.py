dc5a1785fe1ed5bdd64b480269a8c710c2efdf84
eyal darmon <eyal210103eyal@gmail.com>
2022-05-14 12:06:13 +0300

    clean

--- http_request_handler.py
+++ http_request_handler.py
@@ -3,10 +3,11 @@ import logging
 import socketserver
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
-from typing import Tuple
+from typing import Tuple, Callable
 
 from command_handler import CommandHandler
 from exceptions.incompatible_command_type_error import IncompatibleCommandTypeError
+from exceptions.incompatible_retriever_type_error import IncompatibleRetrieverTypeError
 from result import Result
 from retrievers_performers import RetrieversPerformer
 
@@ -25,35 +26,25 @@ def get_request_handler(command_handler: CommandHandler, retriever_performer: Re
             self.wfile.write(result_json.encode(encoding='UTF-16'))  # change to json object
             self.send_header("Content-Length", str(len(result_json)))
 
-        def do_POST(self) -> None:
+        def handle_single_request(self, action: Callable):
             data = self.rfile.read().decode()
             try:
                 json_dict = json.decoder.JSONDecoder().decode(data)
                 logging.info(str(json_dict))
-                result_data = self.command_handler.handle_command(json_dict['type'],
-                                                                  *json_dict['args'] if 'args' in json_dict else None)
+                result_data = action(json_dict['type'] * json_dict['args'] if 'args' in json_dict else None)
                 self.create_ok(result_data)
-            except IncompatibleCommandTypeError as e:
+            except IncompatibleCommandTypeError or IncompatibleRetrieverTypeError as e:
                 logging.error(e)
-                self.send_error(HTTPStatus.BAD_REQUEST, "Bad Command Type")
+                self.send_error(HTTPStatus.BAD_REQUEST, "Bad Type")
             except Exception as e:
                 logging.error(e)
                 self.send_response(HTTPStatus.BAD_REQUEST)
             self.end_headers()
 
+        def do_POST(self) -> None:
+            self.handle_single_request(self.command_handler.handle_command)
+
         def do_GET(self):
-            data = self.rfile.read().decode()
-            try:
-                json_dict = json.decoder.JSONDecoder().decode(data)
-                logging.info(str(json_dict))
-                result_data = self.retriever_performer.perform(json_dict['type'])
-                self.create_ok(result_data)
-            except IncompatibleCommandTypeError as e:
-                logging.error(e)
-                self.send_error(HTTPStatus.BAD_REQUEST, "Bad Retriever Type")
-            except Exception as e:
-                logging.error(e)
-                self.send_response(HTTPStatus.BAD_REQUEST)
-            self.end_headers()
+            self.handle_single_request(self.retriever_performer.perform)
 
     return PCControllerRequestHandler
